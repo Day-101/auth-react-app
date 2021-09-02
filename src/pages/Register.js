bd2c7b4ee45bf159d54891c5afbb53cb9b954823
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-02 22:00:11 +0200

    build: add SignUpForm component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SignUpForm from "../components/Log/SignUpForm"
 
 const Register = () => {
   return (
     <div>
       <h1>Register</h1>
+      <SignUpForm />
     </div>
   );
 };
