675f01b6729650300258edfc7ba45c95af6bedd5
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-02 16:46:42 +0200

    test: check Token and context provider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UidContext } from '../components/AppContext';
 
 const Home = () => {
+  const uid = useContext(UidContext);
+
   return (
     <div>
       <h1>Home</h1>
+      {uid ?(
+        <h1>Voir les users</h1>
+        ) : (
+        <h1>Bienvenue !</h1>
+      )}
     </div>
   );
 };
