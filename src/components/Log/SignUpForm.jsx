71b0fd4dc16d61a6b790420eeb1746170a48506e
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-02 21:58:49 +0200

    feat: login with redirect to login

--- src/components/Log/SignUpForm.jsx
+++ src/components/Log/SignUpForm.jsx
@@ -1,60 +1,110 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const SignUpForm = () => {
+  const history = useHistory();
+  const [formSubmit, setFormSubmit] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [controlPassword, setControlPassword] = useState("");
 
-  const handleLogin = (e) => {
+  const redirectToHome = () => {
+    history.replace("/");
+  };
+
+  const handleRegister = async (e) => {
     e.preventDefault();
+    const terms = document.getElementById("terms");
     const emailError = document.querySelector(".email.error");
     const passwordError = document.querySelector(".password.error");
+    const passwordConfirmError = document.querySelector(
+      ".password-confirm.error"
+    );
+    const termsError = document.querySelector(".terms.error");
 
-    axios({
-      method: "post",
-      url: `${process.env.REACT_APP_API_URL}api/user/login`,
-      withCredentials: true,
-      data: {
-        email,
-        password,
-      },
-    })
-      .then((res) => {
-        console.log(res);
-        if (res.data.errors) {
-          emailError.innerHTML = res.data.errors.email;
-          passwordError.innerHTML = res.data.errors.password;
-        } else {
-          window.location = "/";
-        }
+    passwordConfirmError.innerHTML = "";
+    termsError.innerHTML = "";
+
+    if (password !== controlPassword || !terms.checked) {
+      if (password !== controlPassword)
+        passwordConfirmError.innerHTML =
+          "Les mots de passe ne sont pas identiques.";
+
+      if (!terms.checked)
+        termsError.innerHTML = "Veuillez valider les conditions générales.";
+    } else {
+      await axios({
+        method: "post",
+        url: `${process.env.REACT_APP_API_URL}api/user/register`,
+        data: {
+          email,
+          password,
+        },
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          if (res.data.errors) {
+            emailError.innerHTML = res.data.errors.email;
+            passwordError.innerHTML = res.data.errors.password;
+          } else {
+            setFormSubmit(true);
+            setTimeout(function () {
+              redirectToHome();
+            }, 2000)
+          }
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
-    <form action="" onSubmit={handleLogin} id="sign-up-form">
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        id="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <div className="email error"></div>
-      <label htmlFor="password">Mot de passe</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <div className="password error"></div>
-      <input type="submit" value="Se connecter" />
-    </form>
+    <>
+      {formSubmit ? (
+        <>
+          <h1>Votre compte a bien été créé.</h1>
+          <p>Vous allez être redirigé vers la page de connexion.</p>
+        </>
+      ) : (
+        <form action="" onSubmit={handleRegister} id="sign-up-form">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <div className="email error"></div>
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <div className="password error"></div>
+          <label htmlFor="password-conf">Confirmer le mot de passe</label>
+          <input
+            type="password"
+            name="password"
+            id="password-conf"
+            onChange={(e) => setControlPassword(e.target.value)}
+            value={controlPassword}
+          />
+          <div className="password-confirm error"></div>
+          <input type="checkbox" id="terms" />
+          <label htmlFor="terms">
+            J'accepte les{" "}
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              conditions générales
+            </a>
+          </label>
+          <div className="terms error"></div>
+          <input type="submit" value="Créer un compte" />
+        </form>
+      )}
+    </>
   );
 };
 
