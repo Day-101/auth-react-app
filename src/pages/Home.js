3c05ae3b752e6844e2df5bf0ee84f1eebd645f33
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-04 15:23:59 +0200

    style: in progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { UidContext } from '../components/Contexts/AppContext';
+import Logout from '../components/Log/Logout';
 
 const Home = () => {
   const uid = useContext(UidContext);
+  const history = useHistory();
 
+  useEffect(() => {
+
+  }, [uid]);
+
+  const redirectToLogin = () => {
+    history.replace("/login");
+  };
+  console.log(uid);
   return (
     <div>
-      <h1>Home</h1>
-      {uid ?(
-        <h1>Voir les users</h1>
+      <div>
+      </div>
+      {uid ? (
+        <>
+          <Logout />
+        </>
         ) : (
-        <h1>Bienvenue !</h1>
+        <>
+          <h1>Bienvenue !</h1>
+          <p>Vous êtes sur une application test d'authentification qui a été réalisé avec NodeJS et ReactJS.</p>
+          <p>Day SY</p>
+          <li onClick={redirectToLogin}>Se connecter</li>
+        </>
       )}
     </div>
   );
