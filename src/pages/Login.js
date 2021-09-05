a90f8d173706ca4bea5d728b886dd9aab77dd455
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-05 22:24:38 +0200

    style: move h1 to SignInForm component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,18 @@
 import React from "react";
 import SignInForm from "../components/Log/SignInForm";
+import BtnBack from "components/Buttons/BtnBack";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <SignInForm />
+    <div className="container login">
+      <BtnBack
+        classes="btn-outline btn-back"
+        url="/"
+        content="Retour à l'acceuil"
+      />
+      <div className="content">
+        <SignInForm />
+      </div>
     </div>
   );
 };
