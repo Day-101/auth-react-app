a05ae69ab75d28884256a398ca80525ea9773785
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-05 22:25:50 +0200

    style: add btnBack and refactor

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import SignUpForm from "../components/Log/SignUpForm"
+import React from "react";
+import SignUpForm from "../components/Log/SignUpForm";
+import BtnBack from "components/Buttons/BtnBack";
 
 const Register = () => {
   return (
     <div className="container register">
+      <BtnBack
+        classes="btn-outline btn-back"
+        url="/"
+        content="Retour à l'acceuil"
+      />
       <div className="content">
         <SignUpForm />
       </div>
