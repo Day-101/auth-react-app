7a191acaaf861885bd273a45fe9f6611fc526046
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-06 10:58:38 +0200

    fix: secure users route

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { UidContext } from "../Contexts/AppContext";
 import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import Login from "../../pages/Login";
 import Users from "../../pages/Users";
 
 const Routes = () => {
+  const uid = useContext(UidContext);
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/register" exact component={Register} />
         <Route path="/login" exact component={Login} />
-        <Route path="/users" exact component={Users} />
+        {uid ? <Route path="/users" exact component={Users} /> : <></>}
         <Redirect to="/" />
       </Switch>
     </Router>
